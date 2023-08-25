183b7c52e5b652183fe3c3d161b8b8a58e314b2e
Jinder2050 <yindercondori@gmail.com>
2023-08-24 22:06:29 -0500

    First app end maybe

--- app.py
+++ app.py
@@ -8,10 +8,25 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("EDA en streamlit")
+st.title("EDA en Streamlit")
 
-df = pd.read_csv("src/bike_sharing_dc.csv")
- 
-pyg_html = pyg.walk(df, return_html=True)
- 
-components.html(pyg_html, height=1000, scrolling=True)
+file = st.file_uploader(label='Cargar un archivo CSV', accept_multiple_files=False, type=["csv"])
+if file is not None:
+    try:
+        df = pd.read_csv(file)
+        st.success('Archivo csv cargado correctamente...', icon="✅")
+    except Exception as e:
+        st.error(f"Error al cargar el archivo: {str(e)}")
+else:
+    st.info('Carga un archivo csv para empezar.')
+
+
+if 'df' in locals():
+    with st.spinner("Realizando análisis exploratorio de datos..."):
+        try:
+            pyg_html = pyg.walk(df, return_html=True)
+            components.html(pyg_html, height=1000, scrolling=True)
+        except Exception as e:
+            st.error(f"Error al realizar el análisis exploratorio de datos: {str(e)}")
+
+
